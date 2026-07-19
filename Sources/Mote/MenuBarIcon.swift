61c1e8f7e7d7730e9124af07ce75757ba2fbf671
duncan <duncan@compy386.local>
2026-07-19 19:14:24 +0100

    Tune final menu bar icon size

--- Sources/Mote/MenuBarIcon.swift
+++ Sources/Mote/MenuBarIcon.swift
@@ -15,7 +15,7 @@ enum MenuBarIcon {
   static let statusItemImage: NSImage = {
     let image = (MenuBarIcon.image.copy() as? NSImage) ?? MenuBarIcon.image
     let aspectRatio = image.size.width / image.size.height
-    image.size = NSSize(width: 22 * aspectRatio, height: 22)
+    image.size = NSSize(width: 21 * aspectRatio, height: 21)
     image.isTemplate = true
     return image
   }()
